8eb3e9081d9664cdd88e01b646c43f55e73fdec3
jakejgordon <jakejgordon@gmail.com>
2018-12-24 01:18:34 -0500

    Shrunk grid to 45x45 since it looks nicer

--- CellTakeover/GameSettings.cs
+++ CellTakeover/GameSettings.cs
@@ -2,7 +2,7 @@
 {
     internal static class GameSettings
     {
-        internal static int NumberOfColumnsAndRows = 50;
+        internal static int NumberOfColumnsAndRows = 45;
         internal static int NumberOfCells => NumberOfColumnsAndRows * NumberOfColumnsAndRows;
     }
 }
