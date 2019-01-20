d205d835012433e0e9052ce36db53ae7a63548de
jakejgordon <jakejgordon@gmail.com>
2019-01-20 12:58:49 -0500

    Added API client project

---
+++ FungusToastApiClient/FungusToastApiClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FungusToastApiClient
+{
+    public class FungusToastApiClient
+    {
+        public FungusToastApiClient()
+        {
+
+        }
+
+        public GameState GetGameState(int gameId)
+        {
+            return null;
+        }
+    }
+
+    public class GameState
+    {
+    }
+}
