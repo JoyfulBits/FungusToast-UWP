ea68da38a3c80c83ac134c6c82e1b0d6dddc04e6
jakejgordon <jakejgordon@gmail.com>
2019-01-31 19:05:56 -0500

    return mock data temporarily

--- ApiClient/GamesApiClient.cs
+++ ApiClient/GamesApiClient.cs
@@ -19,29 +19,73 @@ namespace ApiClient
 
         public virtual async Task<GameModel> GetGameState(int gameId, string baseApiUrl)
         {
-            using (var client = new HttpClient())
-            {
-                using (var response = await client.GetAsync(baseApiUrl + "/games/" + gameId))
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        using (var content = response.Content)
-                        {
-                            var data = await content.ReadAsStringAsync();
-                            if (data != null)
-                            {
-                                return _serialization.DeserializeObject<GameModel>(data);
-                            }
-                        }
-                    }
-                }
-            }
+            return MakeMockGameModelForTesting();
+
+            //using (var client = new HttpClient())
+            //{
+            //    using (var response = await client.GetAsync(baseApiUrl + "/games/" + gameId))
+            //    {
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            using (var content = response.Content)
+            //            {
+            //                var data = await content.ReadAsStringAsync();
+            //                if (data != null)
+            //                {
+            //                    return _serialization.DeserializeObject<MakeMockGameModelForTesting>(data);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
             throw new GameNotFoundException(gameId);
         }
 
+        private static GameModel MakeMockGameModelForTesting()
+        {
+            string player1Id = "player 1 id";
+            string player2Id = "player 2 id";
+            return new GameModel
+            {
+                GenerationNumber = 1,
+                Id = 2391,
+                RoundNumber = 1,
+                NumberOfAiPlayers = 1,
+                NumberOfHumanPlayers = 2,
+                NumberOfColumns = 50,
+                NumberOfRows = 50,
+                Status = "Not Started",
+                Players = new List<PlayerState>
+                {
+                    new PlayerState
+                    {
+                        Id = player1Id,
+                        Name = "jake",
+                        Status = "Joined",
+                        Human = true
+                    },
+                    new PlayerState
+                    {
+                        Id = player2Id,
+                        Name = "Moldyman",
+                        Status = "Not Joined",
+                        Human = true,
+                    },
+                    new PlayerState
+                    {
+                        Id = "player id 3",
+                        Name = "AI 1",
+                        Status = "Not Joined",
+                        Human = false,
+                    }
+                }
+            };
+        }
+
         public virtual async Task<GameModel> CreateGame(NewGameRequest newGame, string baseApiUrl)
         {
+            return MakeMockGameModelForTesting();
             using (var client = new HttpClient())
             {
                 var stringifiedObject = _serialization.SerializeToHttpStringContent(newGame);
