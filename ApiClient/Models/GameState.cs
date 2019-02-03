f704902454251e35e5b59ac6171404060cfc5b7a
jakejgordon <jakejgordon@gmail.com>
2019-02-02 23:07:19 -0500

    Added RoundNumber and GenerationNumber to game state

--- ApiClient/Models/GameState.cs
+++ ApiClient/Models/GameState.cs
@@ -5,5 +5,7 @@ namespace ApiClient.Models
     public class GameState
     {
         public Dictionary<int, FungalCell> Cells { get; set; } = new Dictionary<int, FungalCell>();
+        public int RoundNumber { get; set; }
+        public int GenerationNumber { get; set; }
     }
 }
